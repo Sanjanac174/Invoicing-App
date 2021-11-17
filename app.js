63eaa81c0c30471a09b1d5397ec95ff85601d6b0
Sanjanac174 <72245546+Sanjanac174@users.noreply.github.com>
2021-11-17 13:45:25 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const routes = require('./routes')
+const cors = require('cors')
+const debug = require('debug')('invoice-app');
+const path = require('path')
+const morgan = require('morgan');
+
+const app = express();
+debug('app initialized')
+
+const corsOptions = {
+    origin: process.env.CLIENT_URL
+}
+
+app.use(morgan((tokens, req, res) => {
+     return [
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, 'content-length'), '-',
+        tokens['response-time'](req, res), 'ms',
+        JSON.stringify(req.body) // Temporary
+    ].join(' ')
+}))
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({  extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/api', routes);
+
+// app.use('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
+
+app.listen(3000, () => console.log('ok!!!'))
